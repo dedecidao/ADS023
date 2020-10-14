c995f9cb07613fbf84cb6a9ecd8ba10525b27788
dedecidao <dedefest@gmail.com>
2020-10-13 23:39:08 -0300

    Upload Atividade 02

--- Atividades/Atividade02/README.md
+++ Atividades/Atividade02/README.md
@@ -1,3 +1,7 @@
 # Objetivo
 
 - Criar uma versão web do app AlunoOnline utilizando Ionic Framework.
+
+# Resolução:
+
+Foi desenhado um prototipo com navageção entre telas usando componentes do ionic.
---
+++ Atividades/Atividade02/meuapp/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/login', function(req, res, next){
+  res.render('login', { title: 'Aluno Online'});
+});
+
+router.get('/teste', function(req, res, next){
+  res.render('teste', { });
+});
+
+module.exports = router;
